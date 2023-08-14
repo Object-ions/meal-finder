d6f31b473e747e18b574051d92e8ae65b0e660a7
object-ions <moshikoatia@gmail.com>
2023-08-13 23:54:22 -0700

    get DOM elements

---
+++ script.js
@@ -0,0 +1,7 @@
+//Get DOM elements
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+const mealsEl = document.getElementById('meals');
+const resultsHeading = document.getElementById('results-heading');
+const singleMeal = document.getElementById('single-meal');
