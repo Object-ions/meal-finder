46d1c284eb09e9dc27e19e9cc0e0f07c62ee3a16
object-ions <moshikoatia@gmail.com>
2023-08-23 12:01:04 -0600

    add addMealToDOM() that will handle a fetching a meal from the API by its ID and create a card with all the relevent information to display on the DOM

--- script.js
+++ script.js
@@ -60,13 +60,42 @@ function removeMessage() {
 
 //Fetch meal by ID
 function getMealById(mealID) {
-  femealIDtch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
   .then(res => res.json())
   .then(data => {
     const meal = data.meals[0];
 
     addMealToDOM(meal);
   });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
+    }
+  }
+
+  singleMealEl.innerHTML = `
+  <div class ="single-meal">
+    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+    <div class="single-meal-info">
+      ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+      ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+    </div>
+    <div class="main">
+      <p> ${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+      </ul>
+  </div>
+  `;
 }
 
 // Event listeners
