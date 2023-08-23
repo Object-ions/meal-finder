a26bbf4c9fc51addefb8ecf39f7b1db7b2c886af
object-ions <moshikoatia@gmail.com>
2023-08-22 22:14:24 -0600

    re built the main function - searchMeal - API fetching

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Get DOM elements
+// Get DOM elements
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
 const random = document.getElementById('random');
@@ -6,57 +6,40 @@ const mealsEl = document.getElementById('meals');
 const resultsHeading = document.getElementById('result-heading');
 const singleMealEl = document.getElementById('single-meal');
 
-//Searc meal and fetch from API
+// Search meal and fetch from API
 function searchMeal(e) {
   e.preventDefault();
 
-  //Clear single mealEl
+  // Clear single mealEl
   singleMealEl.innerHTML = '';
 
-  //Get search term
+  // Get search term
   const term = search.value;
 
-  //Check for empty
+  // Check for empty
   if (term.trim()) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then(res => res.json())
       .then(data => {
-        console.log(data);
-        resultsHeading.innerHTML = `<h2>Search  results for '${term}':</h2>`;
-
-        if (data.meals === null) {
-          resultsHeading.innerHTML = `<p>there are no search result, try again</p>`
-        } else {
-          mealsEl.innerHTML = data.meals.map(meal => `
-          <div class="meal">
-            <img src="${meal.strMealThumb}" alt="meal.strMeal" />
-            <div class="meal-info" data-mealID="${data.idMeal}">
-            <h3>${meal.strMeal}</h3>
-            </div>
-          </div>
-          `)
-            .join();
-        }
+         resultsHeading.innerHTML = `<h2>Search Results for '${term}'</h2>`;
       });
-    //Clear search content
-    search.value = '';
   } else {
     showErrorMessage();
     setTimeout(removeMessage, 1500)
   }
 }
 
-//Show error message
+// Show error message
 function showErrorMessage() {
   const error = document.createElement('p');
-  error.innerText = 'Please enter a search value';
+  error.innerText = 'Please enter a search term';
   resultsHeading.appendChild(error)
 }
 
-//Remove message
+// Remove message
 function removeMessage() {
   resultsHeading.innerHTML = '';
 }
 
-//Event listeners
+// Event listeners
 submit.addEventListener('submit', searchMeal)
