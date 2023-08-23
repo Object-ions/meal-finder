7fa5273177a7b307627bf7560fb091ddfc7bd50d
object-ions <moshikoatia@gmail.com>
2023-08-22 22:29:54 -0600

    fetch result from API and built a card for the result

--- script.js
+++ script.js
@@ -22,7 +22,23 @@ function searchMeal(e) {
       .then(res => res.json())
       .then(data => {
          resultsHeading.innerHTML = `<h2>Search Results for '${term}'</h2>`;
+
+         if (data.meals === null) {
+          resultsHeading.innerHTML = `<p>No results found for '${term}'</p>`;
+         } else {
+          mealsEl.innerHTML = data.meals.map(meal => `
+            <div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+          `).join('');
+         }
       });
+
+      // Clear search input
+      search.value = '';
   } else {
     showErrorMessage();
     setTimeout(removeMessage, 1500)
